ffd4e281ad7149dbd8f2ee6bfad54f5b279d527a
Irina Dimcheva <dimchevairina@gmail.com>
2024-01-29 23:56:22 +0200

    Initial commit

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,32 @@
+import { ModuleWithProviders, NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { RouterModule } from "@angular/router";
+import { ReactiveFormsModule } from "@angular/forms";
+
+import { MatCardModule } from "@angular/material/card";
+import { MatInputModule } from "@angular/material/input";
+import { MatButtonModule } from "@angular/material/button";
+
+import { LoginComponent } from "./login/login.component";
+import { AuthService } from "./auth.service";
+
+@NgModule({
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatCardModule,
+    MatInputModule,
+    MatButtonModule,
+    RouterModule.forChild([{ path: "", component: LoginComponent }]),
+  ],
+  declarations: [LoginComponent],
+  exports: [LoginComponent],
+})
+export class AuthModule {
+  static forRoot(): ModuleWithProviders<AuthModule> {
+    return {
+      ngModule: AuthModule,
+      providers: [AuthService],
+    };
+  }
+}
